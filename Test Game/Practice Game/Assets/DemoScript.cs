c4360295e61b6e22151dd5dbb41b0c34be2105b5
Joe Thompson <joerfthompson@gmail.com>
2018-06-24 23:13:48 +0100

    Test Game

---
+++ Test Game/Practice Game/Assets/DemoScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour
+{
+    [SerializeField]
+    public Light myLight;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+
+} // 21:14 https://unity3d.com/learn/tutorials/topics/scripting/coding-unity-absolute-beginner
