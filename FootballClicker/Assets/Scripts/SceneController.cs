ded12bdcd8153c02e3be2b90f2aba7058d22621d
jonathan7ben <jonathangrace321@gmail.com>
2018-06-28 00:24:57 +0100

    scene changer fix

--- FootballClicker/Assets/Scripts/SceneController.cs
+++ FootballClicker/Assets/Scripts/SceneController.cs
@@ -6,23 +6,6 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
 
-    public static SceneController _instance;
-    private void Awake()
-    {
-        // Instance of scene controller start.
-        if (_instance == null)
-        {
-            _instance = this;
-        }
-        else if (_instance != this)
-        {
-            Destroy(gameObject);
-        }
-
-        DontDestroyOnLoad(gameObject);
-        // Instance of scene controller end.
-    }
-
     public void SwapScene(int sceneIndex)
     {
         SceneManager.LoadSceneAsync(sceneIndex);
