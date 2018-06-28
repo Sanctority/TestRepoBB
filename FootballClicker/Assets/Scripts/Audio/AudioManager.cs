922e1ed26fd082fba06a9ab1995ddc3eb12ab42f
QuiteUnhappy <wj.middleton93@gmail.com>
2018-06-28 13:40:38 +0100

    Audio manager changes and some tidying

--- FootballClicker/Assets/Scripts/Audio/AudioManager.cs
+++ FootballClicker/Assets/Scripts/Audio/AudioManager.cs
@@ -1,66 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
-
     // Public variables
-    public static AudioManager _instance = null;
-
-    public List<AudioClip> _audioClips;
+    public AudioClip _mainMenuMusic;                // Main menu audio clip
+    public AudioClip _gameMusic;                    // Main game audio clip
 
     // Private variables
-    private AudioSource _audioSource;
-    private int _audioClipsCounter;
-
-    //Singleton
-    private void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this;
-        }
-        else if (_instance != this)
-        {
-            Destroy(gameObject);
-        }
-
-        DontDestroyOnLoad(gameObject);
-    }
+    private AudioSource _audioSource;               
 
     private void Start()
     {
-        _audioClipsCounter = 0;
         _audioSource = GetComponent<AudioSource>();
-        _audioSource.volume = 0.4f;
+        _audioSource.volume = 0.2f;                         // Set audio to a low amount, its still loud, might want to clamp it
+        _audioSource.loop = true;                           // Set audio to loop
+        _audioSource.clip = _mainMenuMusic;                 // Audio Source will load on main menu when game loads, which does not call the activeSceneChanged event, so assign it here
+        DontDestroyOnLoad(gameObject);                      // Make sure the Audio Source stays alive through scene changes
+        SceneManager.activeSceneChanged += SceneChanged;    // Subscribe to the activeSceneChanged event
     }
-    private void Update()
-    {
-        if (!_audioSource.isPlaying)
-        {
-            PlayNextTrack();
-        }
 
+    private void SceneChanged(Scene current, Scene next)
+    {
+        if (next.buildIndex == 0)               // Check if next scene is Main menu
+        {
+            _audioSource.Stop();
+            _audioSource.clip = _mainMenuMusic; // Change the clip
+            _audioSource.Play();
+        }
+        else 
+        if (next.buildIndex == 1)               // Check if scene is Main game
+        {
+            _audioSource.Stop();
+            _audioSource.clip = _gameMusic;     // Change the clip
+            _audioSource.Play();
+        }
     }
 
     public void ChangeVolume(float _vol)
     {
-        _audioSource.volume = _vol;
-    }
-
-    public void PlayNextTrack()
-    {
-        _audioSource.clip = _audioClips[_audioClipsCounter];
-        _audioSource.PlayDelayed(0.25f);
-
-        if (_audioClipsCounter+1 >=  _audioClips.Count)
-        {
-            _audioClipsCounter = 0;
-        }
-        else
-        {
-            _audioClipsCounter++;
-        }
+        _audioSource.volume = _vol;             // Exposed function for setting volume which we can clamp
     }
 }
