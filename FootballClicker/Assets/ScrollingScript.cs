925a49b8acf71c2dc46f5a9acb019965c117a13d
jonathan7ben <jonathangrace321@gmail.com>
2018-07-04 16:12:16 +0100

    scrolling background
    complete

---
+++ FootballClicker/Assets/ScrollingScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingScript : MonoBehaviour {
+
+    public float _scrollSpeed;
+
+
+    private void Update()
+    {
+        Vector2 _change = new Vector2(Time.time * _scrollSpeed, 0);
+
+        GetComponent<Renderer>().material.mainTextureOffset = _change;
+    }
+}
