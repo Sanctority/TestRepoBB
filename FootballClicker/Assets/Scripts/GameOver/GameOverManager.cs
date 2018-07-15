c2712fe01cc5676fb744d186d76b9898d5994978
Joe Thompson <joerfthompson@gmail.com>
2018-07-15 22:47:18 +0100

    Update

--- FootballClicker/Assets/Scripts/GameOver/GameOverManager.cs
+++ FootballClicker/Assets/Scripts/GameOver/GameOverManager.cs
@@ -7,19 +7,36 @@ public class GameOverManager : MonoBehaviour {
 
     // Public variables.
     public TextMeshProUGUI _highscoreDisplayText;       // This will store a reference to the highscore text for use.
+    public TextMeshProUGUI _previousDisplayText;
+    public TextMeshProUGUI _gemsDisplayText;
 
     // Private variables.
     private GameManager _GM;                            // This will store a reference to the GameManager.
+    private int _gemsEarned;
+    private int _numOfBounces;
 
     private void Start()
     {
         _GM = GameManager._instance;
+
+        _gemsEarned = PlayerPrefs.GetInt("GemCount");
+        _numOfBounces = PlayerPrefs.GetInt("Bounces");
+
         UpdateHighscoreText();
     }
 
     // This funciton is used to refresh the highscore text.
     private void UpdateHighscoreText()
     {
-        _highscoreDisplayText.text = "Highscore: " + PlayerPrefs.GetInt("Highscore").ToString(); ;
+        _highscoreDisplayText.text = "Highscore: " + PlayerPrefs.GetInt("Highscore").ToString();
+        _previousDisplayText.text = "Previous score: " + PlayerPrefs.GetInt("PreviousScore").ToString(); ;
+        _gemsDisplayText.text = "Gems earned: " + _gemsEarned.ToString();
+
+        _GM.UpdateGems(_gemsEarned);
+
+    #if UNITY_ANDROID
+        GooglePlayGamesScript.IncrementAchievement(GPGSIds.achievement_kicking_noob, _numOfBounces);
+        GooglePlayGamesScript.IncrementAchievement(GPGSIds.achievement_kicking_novice, _numOfBounces);
+    #endif
     }
 }
--- FootballClicker/Assets/Scripts/LevelScripts/BallScript.cs
+++ FootballClicker/Assets/Scripts/LevelScripts/BallScript.cs
@@ -21,6 +21,8 @@ public class BallScript : MonoBehaviour
     private Rigidbody2D _ballRB;
     private int _numOfBouncesUp;
     private int _numOfBouncesDown;
+    private int _numOfBouncesMade;
+    private int _gemCount;
 
     
 
@@ -30,7 +32,7 @@ public class BallScript : MonoBehaviour
         _ballRB = GetComponent<Rigidbody2D>();
         _ballRB.sharedMaterial = _physicsHasBounce;
         _numOfBouncesUp = 0;
-
+        _numOfBouncesMade = 0;
         
     }
 
@@ -99,21 +101,27 @@ public class BallScript : MonoBehaviour
             _numOfBouncesUp = 0;
             _numOfBouncesDown = 0;
         }
+
+        if (collision.gameObject.tag == "Gem")
+        {
+            Destroy(collision.gameObject);
+            _gemCount++;
+        }
+
     }
 
     private void GameOver()
     {
         _uiCanvas.gameObject.GetComponent<MainLevel.UiScript>().GameOver();
+        PlayerPrefs.SetInt("Bounces",_numOfBouncesMade);
+        PlayerPrefs.SetInt("GemCount",_gemCount);
         SceneManager.LoadScene("GameOver");
     }
 
     // This will be used to handle the achievments for kicking the ball
     private void AchievementIncrementKicker()
     {
-#if UNITY_ANDROID
-        GooglePlayGamesScript.IncrementAchievement(GPGSIds.achievement_kicking_noob, 1);
-        GooglePlayGamesScript.IncrementAchievement(GPGSIds.achievement_kicking_novice, 1);
-#endif
+        _numOfBouncesMade++;
     }
 
     private void BallRotation()
