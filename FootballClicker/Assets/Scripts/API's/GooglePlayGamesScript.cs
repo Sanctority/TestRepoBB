ee41e5a518ed906a3dd6d1a15bf966a9130a5688
jonathan7ben <40474910+jonathan7ben@users.noreply.github.com>
2018-12-03 18:58:02 +0000

    Revert "Revert "ay?""
    This reverts commit 404bb0e7d04af6baf01792f3ab082836d2c880fe.

--- FootballClicker/Assets/Scripts/API's/GooglePlayGamesScript.cs
+++ FootballClicker/Assets/Scripts/API's/GooglePlayGamesScript.cs
@@ -12,6 +12,15 @@ public class GooglePlayGamesScript : MonoBehaviour {
 
     public static GooglePlayGamesScript Instance { get; private set; }
 
+<<<<<<< HEAD
+=======
+    const string _saveName = "BouncyBoySave";
+    const string _gemSave = "PlayersGems";
+
+    bool _isSaving;
+
+    bool _isGemCloudDataLoaded;
+>>>>>>> parent of de98bc4... cloud save added
 
     private void Start()
     {
@@ -37,7 +46,11 @@ public class GooglePlayGamesScript : MonoBehaviour {
 
     private void SignIn()
     {
+<<<<<<< HEAD
         Social.localUser.Authenticate(success => {  GameManager._instance.PlayerIsOnline();  });
+=======
+        Social.localUser.Authenticate(success => {});
+>>>>>>> parent of de98bc4... cloud save added
     }
 
     public static bool CheckIfLoggedIn()
@@ -101,11 +114,138 @@ public class GooglePlayGamesScript : MonoBehaviour {
 
     #endregion LeaderboardsEnd
 
+<<<<<<< HEAD
     #region save
 
 
 
     #endregion
+=======
+    #region Saved Games
 
+    private string GemsToString()
+    {
+        return GameManager._instance.ReturnGems().ToString();
+    }
+
+    void GemsStringToGameData(string cloudData, string localData)
+    {
+        if(PlayerPrefs.GetInt("FirstTimeSave") == 1)
+        {
+            PlayerPrefs.SetInt("FirstTimeSave", 0);
+
+            if(int.Parse(localData) > int.Parse(cloudData))
+            {
+                GameManager._instance.SetGems(int.Parse(localData));
+                _isGemCloudDataLoaded = true;
+                SaveData();
+                return;
+            }
+        }
+        else
+        {
+            GameManager._instance.SetGems(int.Parse(cloudData));
+            _isGemCloudDataLoaded = true;
+        }
+        
+    }
+
+    void GemsStringToGameData(string localData)
+    {
+        _isGemCloudDataLoaded = false;
+        GameManager._instance.SetGems(0);
+    }
+
+    public void GemsLoadData()
+    {
+        if (Social.localUser.authenticated)
+        {
+            _isSaving = false;
+            ((PlayGamesPlatform)Social.Active).SavedGame.OpenWithManualConflictResolution(_gemSave, DataSource.ReadCacheOrNetwork, true, GemsResolveConflict, GemsSaveOpened);
+        }
+
+    }
+
+    public void SaveData()
+    {
+        if (!_isGemCloudDataLoaded)
+        {
+            return;
+        }
+
+        if (Social.localUser.authenticated)
+        {
+            _isSaving = true;
+            ((PlayGamesPlatform)Social.Active).SavedGame.OpenWithManualConflictResolution(_gemSave, DataSource.ReadCacheOrNetwork, true, GemsResolveConflict, GemsSaveOpened);
+        }
+    }
+
+    private void GemsResolveConflict(IConflictResolver resolver,ISavedGameMetadata original,byte[] originalData,ISavedGameMetadata unmerged, byte[] unmergedData)
+    {
+        if(originalData == null)
+        {
+            resolver.ChooseMetadata(unmerged);
+        }
+        else if(unmergedData == null)
+        {
+            resolver.ChooseMetadata(original);
+        }
+        else
+        {
+            resolver.ChooseMetadata(original);
+            return;
+        }
+
+        resolver.ChooseMetadata(original);
+    }
+
+    void ShowSelectUI()
+    {
+        uint maxNumToDisplay = 1;
+        bool allowCreateNew = false;
+        bool allowDelete = true;
+
+        ISavedGameClient savedGameClient = PlayGamesPlatform.Instance.SavedGame;
+        savedGameClient.ShowSelectSavedGameUI("Select saved game",
+            maxNumToDisplay,
+            allowCreateNew,
+            allowDelete,
+            OnSavedGameSelected);
+    }
+
+
+    public void OnSavedGameSelected(SelectUIStatus status, ISavedGameMetadata game)
+    {
+        if (status == SelectUIStatus.SavedGameSelected)
+        {
+            // handle selected game save
+        }
+        else
+        {
+            // handle cancel or error
+        }
+    }
+
+    void OpenSavedGame(string filename)
+    {
+        ISavedGameClient savedGameClient = PlayGamesPlatform.Instance.SavedGame;
+        savedGameClient.OpenWithAutomaticConflictResolution(filename, DataSource.ReadCacheOrNetwork,
+            ConflictResolutionStrategy.UseLongestPlaytime, OnSavedGameOpened);
+    }
+
+    public void OnSavedGameOpened(SavedGameRequestStatus status, ISavedGameMetadata game)
+    {
+        if (status == SavedGameRequestStatus.Success)
+        {
+            // handle reading or writing of saved game.
+        }
+        else
+        {
+            // handle error
+        }
+    }
+>>>>>>> parent of de98bc4... cloud save added
+
+    #endregion Saved Games End
 #endif
 }
